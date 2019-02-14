8c4ac95c1c6b6702efb1593b6664fe3f1bf2cc34
Austin Melms <austin@melmsie.com>
2019-02-13 21:52:20 -0500

    unused function

--- server.js
+++ server.js
@@ -211,18 +211,6 @@ function validatePatreonIdentity (req) {
   return crypted === hash;
 }
 
-function formatTime (time) { // eslint-disable-line no-unused-vars
-  let days = Math.floor(time % 31536000 / 86400);
-  let hours = Math.floor(time % 31536000 % 86400 / 3600);
-  let minutes = Math.floor(time % 31536000 % 86400 % 3600 / 60);
-  let seconds = Math.round(time % 31536000 % 86400 % 3600 % 60);
-  days = days > 9 ? days : '0' + days;
-  hours = hours > 9 ? hours : '0' + hours;
-  minutes = minutes > 9 ? minutes : '0' + minutes;
-  seconds = seconds > 9 ? seconds : '0' + seconds;
-  return `${days > 0 ? `${days}:` : ``}${(hours || days) > 0 ? `${hours}:` : ``}${minutes}:${seconds}`;
-}
-
 function getUser (id, amount) {
   return {
     id, // User id/rethink id
