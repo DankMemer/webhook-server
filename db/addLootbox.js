de54a46f907e951d9bd85bc12b8f7f8bb4a4c55f
aetheryx <zainalih3c@gmail.com>
2019-02-17 00:59:43 +0100

    update addLootbox to accept different kinds of lootboxes

--- db/addLootbox.js
+++ db/addLootbox.js
@@ -2,10 +2,10 @@ const _saveQuery = require('./_saveQuery.js');
 const _fetchUserQuery = require('./_fetchUserQuery.js');
 const r = require('./r.js');
 
-module.exports = async function addLootbox (id) {
+module.exports = async function addLootbox (id, type = 'normie') {
   return _saveQuery(_fetchUserQuery(id).merge({
     inventory: {
-      normie: r.row('inventory').default({}).getField('normie').default(0).add(1)
+      [type]: r.row('inventory').default({}).getField(type).default(0).add(1)
     },
     upvoted: true
   })).run();
