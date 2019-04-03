553deebcdc76c83ef12a931410383e5607b18d56
Aetheryx <zain1@live.nl>
2019-04-03 19:57:26 +0200

    add purchase timestamps

--- db/addLootbox.js
+++ db/addLootbox.js
@@ -4,6 +4,7 @@ const r = require('./r.js');
 
 module.exports = async function addLootbox (id, type = 'normie', amount = 1) {
   return _saveQuery(_fetchUserQuery(id).merge({
+    purchasedBox: Date.now(),
     inventory: {
       [type]: r.row('inventory').default({}).getField(type).default(0).add(amount)
     },
