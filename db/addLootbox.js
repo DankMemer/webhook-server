c1321377073daa54c68923ee82219a285c03bf69
Austin Melms <memyselfandlie@gmail.com>
2020-09-06 11:41:26 -0400

    update box type

--- db/addLootbox.js
+++ db/addLootbox.js
@@ -3,14 +3,14 @@ const _fetchUserQuery = require('./_fetchUserQuery.js');
 const r = require('./r.js');
 const redis = require('./redis');
 
-module.exports = async function addLootbox (id, type = 'normie', amount = 1) {
+module.exports = async function addLootbox (id, type = 'meme', amount = 1) {
   redis.hdel('user-entries', id);
 
   return _saveQuery(_fetchUserQuery(id).merge({
     inventory: {
       [type]: r.row('inventory').default({}).getField(type).default(0).add(amount)
     },
-    ...(type === 'normie' && amount === 1
+    ...(type === 'meme' && amount === 1
       ? { upvoted: true }
       : { purchasedBox: Date.now() }
     )
