5be836c294a16e988d583caa75d5e5b7dae98058
Melmsie <austin@melmsie.com>
2020-12-31 18:53:08 -0500

    new vote rewards

--- endpoints/dblist.js
+++ endpoints/dblist.js
@@ -14,8 +14,8 @@ module.exports = (app, config) =>
     ) {
       const body = JSON.parse(req.body);
   
-      await addVote(body.id, 5000, 'gift', 'normie', 2, true);
-      await sendNotification(body.id, 'vote', 'Thank you for voting!', 'You just got your **`2 normie boxes, 2 gifts, and 5k coins`** for voting on discordbotlist.com!');
+      await addVote(body.id, 5000, 'pizza', 'banknote', 1, true);
+      await sendNotification(body.id, 'vote', 'Thank you for voting!', 'You just got your **`1 banknote, 1 pizza, and 5k coins`** for voting on discordbotlist.com! (Extra 4k coins for supporting this growing site)');
       ddog.increment(`webhooks.dblcom`);
       res.status(200).send({ status: 200 });
     } else {
