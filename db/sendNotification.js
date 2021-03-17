5462fad83d8a37859d3d45e5dfd66215cc92d974
aetheryx <zainalih3c@gmail.com>
2021-03-17 15:50:48 +0100

    fix notifications

--- db/sendNotification.js
+++ db/sendNotification.js
@@ -1,11 +1,27 @@
 const _saveQuery = require('./_saveQuery.js');
 const _fetchUserQuery = require('./_fetchUserQuery.js');
 const r = require('./r.js');
+const redis = require('./redis');
 
 module.exports = async function sendNotification (id, type, title, message) {
+  const notification = {
+    type,
+    title,
+    message,
+    timestamp: Date.now()
+  };
+
+  await redis.publish(
+    'changes:user:0:notification',
+    JSON.stringify({
+      id,
+      data: notification
+    })
+  );
+
   return _saveQuery(_fetchUserQuery(id).merge({
     notifications: r.row('notifications')
       .default([])
-      .append({ type, title, message, timestamp: Date.now() })
+      .append(notification)
   })).run();
 };
