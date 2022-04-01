fe081c696050f53b70d3d7857b779152fe9ebe4b
Melmsie <memyselfandlie@gmail.com>
2022-04-01 16:51:11 -0400

    Update dbl.js

--- endpoints/dbl.js
+++ endpoints/dbl.js
@@ -34,11 +34,11 @@ module.exports = (app, config) =>
 async function handleWebhook(body) {
  if (body.isWeekend) {
       ddog.increment(`webhooks.topgg.memer`);
-      await addVote(body.user, 20000, 'adventureticket', 'daily', 2, false);
-      await sendNotification(body.user, 'vote', 'Thank you for voting!', 'You just got your **`2 Adventure Tickets, 1 Daily box, and 20k coins`** for voting on top.gg!');
+      await addVote(body.user, 50000, 'banknote', 'daily', 8, false);
+      await sendNotification(body.user, 'vote', 'Thank you for voting!', 'You just got your **`8 Banknotes, 1 Daily box, and 50k coins`** for voting on top.gg!');
     } else {
       ddog.increment(`webhooks.topgg.memer`);
-      await addVote(body.user, 10000, 'adventureticket', 'daily', 1, false);
-      await sendNotification(body.user, 'vote', 'Thank you for voting!', 'You just got your **`Adventure Ticket, Daily box, and 10k coins`** for voting on top.gg!');
+      await addVote(body.user, 25000, 'banknote', 'daily', 4, false);
+      await sendNotification(body.user, 'vote', 'Thank you for voting!', 'You just got your **`4 Banknotes, 1 Daily box, and 25k coins`** for voting on top.gg!');
     }
 }
